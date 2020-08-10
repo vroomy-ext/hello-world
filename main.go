c7288d783a0595abc919884f31ead3b9611d6ca9
Josh Montoya <josh@montoya.io>
2020-08-10 10:09:54 -0700

    implement load func

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
+import "github.com/vroomy/common"
+
 // Init will be called by vroomy on initialization
 func Init(env map[string]string) (err error) {
 	// Plugin initialization occurs here
+	return
+}
+
+// Load will be called by vroomy after plugin initialization
+func Load(p common.Plugins) (err error) {
+	// Referencing other plugins occurs here
 	return
 }
