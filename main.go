726458e427cdae32ab0890db97bab240ad11bbca
Josh Montoya <josh@montoya.io>
2020-08-12 00:07:53 -0700

    implement New handler

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/Hatch1fy/httpserve"
 	helloworld "github.com/gdbu/hello-world"
 	"github.com/vroomy/common"
 )
@@ -33,6 +34,32 @@ func Load(p common.Plugins) (err error) {
 func Backend() interface{} {
 	// Return hello world controller
 	return hw
+}
+
+// New is the handler for creating a new Entry
+func New(ctx *httpserve.Context) (res httpserve.Response) {
+	var (
+		e   helloworld.Entry
+		err error
+	)
+
+	// Parse request body as JSON
+	if err = ctx.BindJSON(&e); err != nil {
+		// Error parsing request body, return error
+		err = fmt.Errorf("error parsing request body: %v", err)
+		return httpserve.NewJSONResponse(400, err)
+	}
+
+	var createdID string
+	// Attempt to insert parsed Entry into the helloworld.Controller
+	if createdID, err = hw.New(e); err != nil {
+		// Error inserting new Entry, return error
+		err = fmt.Errorf("error creating new entry: %v", err)
+		return httpserve.NewJSONResponse(400, err)
+	}
+
+	// Entry was successfully inserted, return newly created ID
+	return httpserve.NewJSONResponse(200, createdID)
 }
 
 // Close will close the plugin
